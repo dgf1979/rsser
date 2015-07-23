6c5bb43c307c743c3711db32f936c41cd8b58457
dgf1979 <drew@finstrom.us>
2015-07-22 20:08:44 -0700

    add default sort orders title ASC on feed, pub_date DESC on item

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,4 +1,5 @@
 class Feed < ActiveRecord::Base
+  default_scope { order('title ASC') }
   validates :rss, presence: true, uniqueness: true
   validates :title, presence: true
   has_many :items
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
+  default_scope { order('pub_date DESC') }
   validates :mp3_url, presence: true
   validates :feed_id, presence: true
   validates :title, presence: true, uniqueness: { scope: :feed,
