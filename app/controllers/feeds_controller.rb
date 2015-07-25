cf89731631f2f1dfc0cfda06ae7e4e023a210350
dgf1979 <drew@finstrom.us>
2015-07-24 19:25:56 -0700

    fix render - this time for real

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -26,7 +26,7 @@ class FeedsController < ApplicationController
       end
     end
     flash[:notice] = 'Feeds parsed and loaded.'
-    render :feeds_new_bulkload
+    render 'feeds/new_bulkload'
   end
 
   def catchup
